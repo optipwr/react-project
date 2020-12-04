5b1ffb266f66639c51edf630be487af578932767
Michael Irby <michael.irbstoomuch@gmail.com>
2020-12-04 09:28:59 -0500

    form expansion/validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>;
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
